6b9c63251520709d86bb53468954f01f34d0f0ae
Rafi <ahmadrafiin467@gmail.com>
2025-06-21 23:17:13 +0800

    Fix Alert Massages issues

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowerCase = "abcdefghijklmnopqrstuvwxyz";
 let number = "1234567890";
-let specialCharacter = "!@#$%^&*()_+";
+let specialCharacter = "!@#$%^&*()_+-=<>?/[]{}";
 
-let password
+let password;
 
 
 
@@ -77,31 +77,60 @@ function generatePassword(){
         let randomIndexPicker;
         let passwordConcatLength = passwordConcat.length;
         password = "";
+        console.log(passwordLength)
         if(passwordLength){
-            console.log(passwordLength)
+            
             if(passwordLength <= 100){
                 for(let x = 1; x <= passwordLength; x++){
                     randomIndexPicker = Math.floor(Math.random()*passwordConcatLength);
                     password+=passwordConcat[randomIndexPicker];
                 }
+                document.getElementById("passwordResult").classList.remove("alertResult");
+                document.getElementById("passwordResult").textContent = password;
 
+                document.getElementById("copy").innerHTML = '<button onclick="copyPassword()" id="copyButton">Copy Password</button>';
             } 
             else {
-                password = "Your Password is too long!"
+                
+                try{
+                    document.getElementById("copyButton").remove();
+                    document.getElementById("passwordResult").classList.add("alertResult") 
+                    document.getElementById("passwordResult").textContent = "Your Password is too long!"
+                }
+                catch {
+                    console.log("Error")
+                    document.getElementById("passwordResult").classList.add("alertResult") 
+                    document.getElementById("passwordResult").textContent = "Your Password is too long!"
+                }
+                
             }
-        } else {
-            password = "Input Password Length First!"
+        } 
+        else {
+            
+            try{
+                   document.getElementById("copyButton").remove();
+                   document.getElementById("passwordResult").classList.add("alertResult") 
+                   document.getElementById("passwordResult").textContent = "Input Password Length First!"
+               }
+               catch {
+                   console.log("Error")
+                   document.getElementById("passwordResult").classList.add("alertResult") 
+                   document.getElementById("passwordResult").textContent = "Input Password Length First!"
+               }
         }
-
-
-        
-        document.getElementById("passwordResult").textContent = password;
-        document.getElementById("copyButton").innerHTML = '<button onclick="copyPassword()">Copy Password</button>';
-        return password;
         
         
     } catch {
+        document.getElementById("passwordResult").classList.add("alertResult") 
         document.getElementById("passwordResult").textContent = "Choose the included element first!"
+        try{
+            document.getElementById("copyButton").remove();
+            console.log("try")
+        }
+        catch {
+            console.log("catch")
+        }
+        
     }
 
     
